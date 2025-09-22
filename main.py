10cfbd9d0b6d6f98c19c37fa46e57cd245653e85
Давид <rubaevd12@gmail.com>
2025-09-22 12:54:14 +0300

    add main page route

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from fastapi import FastAPI
 
+
+app = FastAPI()
+
+
+@app.get("/")
+def main_page():
+    return {"message": "Welcome to main page!"}
+
+
+@app.get("/data")
+def all_data():
+    return {"message": "Типа вывелась вся бд"}
